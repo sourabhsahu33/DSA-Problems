dc1c748e7102fb8584299fe65107f5a9113751c8
Sourabh Sahu <104298270+Sourabhsahu33@users.noreply.github.com>
2023-06-21 15:23:33 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Reverse Coding/reverse-coding.java
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S[] = read.readLine().split(" ");
+            
+            int n = Integer.parseInt(S[0]);
+
+            Solution ob = new Solution();
+            System.out.println(ob.sumOfNaturals(n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static final int MOD = 1000000007;
+    static int sumOfNaturals(int n) {
+        // code here
+        return  (int)(((n%MOD) * 1L * ((n+1)%MOD) /2) % MOD);
+    }
+};
