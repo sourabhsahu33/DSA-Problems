aafc5940084067b3890d154a2e5aed6894050a38
Sourabh Sahu <104298270+Sourabhsahu33@users.noreply.github.com>
2023-09-13 00:11:17 +0530

    Updated Solution - GfG to GitHub

--- Easy/Perfect Numbers/perfect-numbers.java
+++ Easy/Perfect Numbers/perfect-numbers.java
@@ -24,17 +24,15 @@ class GFG {
 
 class Solution {
     static int isPerfectNumber(long N) {
-        if (N <= 1) {
-            return 0;  
+        if( N<=1){
+            return 0;
         }
-        
-        long sum = 1;  
-
-        for (long i = 2; i * i <= N; i++) {
-            if (N % i == 0) {
+        long sum = 1;
+        for(long i = 2; i*i<=N; i++){
+            if(N%i == 0){
                 sum += i;
-                if (i != N / i) {
-                    sum += N / i;
+                if( i != N/i){
+                    sum += N/i;
                 }
             }
         }
