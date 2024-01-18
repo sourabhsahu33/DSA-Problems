0a91c89540dbbaa944c11b99cff8d19f5e5649f1
Sourabh Sahu <104298270+sourabhsahu33@users.noreply.github.com>
2024-01-18 19:15:03 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Maximum subset XOR/maximum-subset-xor.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+import java.util.*;
+
+import java.io.*;
+class XOR
+{	
+	public static void main(String[] args) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-- > 0)
+		{
+		  int n = Integer.parseInt(br.readLine().trim());
+		  String s[] = br.readLine().trim().split(" ");
+		  int[] a = new int[n];
+			for(int i=0;i<n;i++)
+				a[i]=Integer.parseInt(s[i]);
+		  Solution g = new Solution();
+		  System.out.println(g.maxSubsetXOR(a , n));
+		}
+	}
+}
+// } Driver Code Ends
+
+
+/*Complete the function below*/
+
+class Solution
+{
+    public static int maxSubsetXOR(int arr[], int n)
+    {
+        //add code here.
+        if(n == 0) return 0;
+        int num = 0;
+        while(true){
+            int max = Integer.MIN_VALUE;
+            for(int i=0; i<n; i++){
+                if( max < arr[i]) 
+                max = arr[i];
+            }
+            if(max == 0) return num;
+            
+            num = Math.max(num, num^max);
+            for(int i=0; i<n; i++){
+                arr[i] = Math.min(arr[i], arr[i]^ max);
+            }
+        }
+    }
+}
