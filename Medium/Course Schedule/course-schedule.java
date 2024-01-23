75b4386e14f10af6d1003326463c887e68880d99
Sourabh Sahu <104298270+sourabhsahu33@users.noreply.github.com>
2024-01-23 17:55:18 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Course Schedule/course-schedule.java
@@ -0,0 +1,109 @@
+//{ Driver Code Starts
+//Initial template for JAVA
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+            ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+            String st[] = read.readLine().trim().split("\\s+");
+            int n = Integer.parseInt(st[0]);
+            int m = Integer.parseInt(st[1]);
+
+            for (int i = 0; i < n; i++)
+                list.add(i, new ArrayList<Integer>());
+
+            ArrayList<ArrayList<Integer>> prerequisites = new ArrayList<>();
+            for (int i = 1; i <= m; i++) {
+                String s[] = read.readLine().trim().split("\\s+");
+                int u = Integer.parseInt(s[0]);
+                int v = Integer.parseInt(s[1]);
+                list.get(v).add(u);
+                ArrayList<Integer> pair = new ArrayList<>();
+                pair.add(u);
+                pair.add(v);
+                prerequisites.add(pair);
+            }
+
+            int[] res = new Solution().findOrder(n, m, prerequisites);
+            
+            if(res.length==0)
+                System.out.println("No Ordering Possible");
+            else
+            {
+                if (check(list, n, res) == true)
+                    System.out.println("1");
+                else
+                    System.out.println("0");
+            }
+        }
+    }
+    static boolean check(ArrayList<ArrayList<Integer>> list, int V, int[] res) {
+        int[] map = new int[V];
+        for (int i = 0; i < V; i++) {
+            map[res[i]] = i;
+        }
+        for (int i = 0; i < V; i++) {
+            for (int v : list.get(i)) {
+                if (map[i] > map[v]) return false;
+            }
+        }
+        return true;
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    static int[] findOrder(int n, int m, ArrayList<ArrayList<Integer>> prerequisites) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            adj.add(new ArrayList<Integer>());
+        }
+        for(ArrayList<Integer> preReqList : prerequisites) {
+            int course = preReqList.get(0);
+            int prerequisite = preReqList.get(1);
+            adj.get(prerequisite).add(course);
+        }
+        
+        int indegree[] = new int[n];
+        for (ArrayList<Integer> preReqList : prerequisites) {
+            int course = preReqList.get(0);
+            indegree[course]++;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) {
+                q.offer(i);
+            }
+        }
+
+        int topo[] = new int[n];
+        int idx = 0;
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            topo[idx++] = node;
+
+            for (int it : adj.get(node)) {
+                indegree[it]--;
+                if (indegree[it] == 0) {
+                    q.offer(it);
+                }
+            }
+        }
+        
+        if (idx == n) {
+            return topo;
+        }
+        return new int[0];
+    }
+}
