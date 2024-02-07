c81663efb62b1acafafd3bd8e5f3d7e79c2c2a4b
Sourabh Sahu <104298270+sourabhsahu33@users.noreply.github.com>
2024-02-07 15:59:04 +0530

    Updated Solution - GfG to GitHub

--- Medium/Count ways to reach the n'th stair/count-ways-to-reach-the-nth-stair.java
+++ Medium/Count ways to reach the n'th stair/count-ways-to-reach-the-nth-stair.java
@@ -31,20 +31,23 @@ class GFG {
 // } Driver Code Ends
 
 
+
+
 class Solution
 {
     //Function to count number of ways to reach the nth stair.
     int countWays(int n)
     {
-       int a = 1, b = 2;
-       int mod = 1000000007;
-       if(n < 3) return n;
-       for(int i=3; i<=n; i++){
-           int temp = a;
-           a = b % mod;
-           b = (temp + a) % mod;
+        int mod = 1000000007;
+       if(n <=1) return n;
+       int dp[] = new int[n+1];
+       
+       dp[0] = 1;
+       dp[1] = 1;
+       for(int i=2; i<=n; i++){
+           dp[i] = (dp[i-1]+dp[i-2]) % mod;
        }
-       return b;
+       return dp[n] % mod;
     }
 }
 
