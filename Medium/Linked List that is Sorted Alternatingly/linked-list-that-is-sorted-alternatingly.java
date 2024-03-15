5d8e85b8ee2865b2ae7189189e531725a62e958c
Sourabh Sahu <104298270+sourabhsahu33@users.noreply.github.com>
2024-03-15 23:40:00 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Linked List that is Sorted Alternatingly/linked-list-that-is-sorted-alternatingly.java
@@ -0,0 +1,87 @@
+//{ Driver Code Starts
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+    
+    public Node (int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class GFG {
+    static void printList(Node node) 
+	{ 
+		while (node != null) 
+		{ 
+			System.out.print(node.data + " "); 
+			node = node.next; 
+		} 
+		System.out.println(); 
+	}
+	public static void main (String[] args) {
+		Scanner sc  = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-- > 0){
+		    int n = sc.nextInt();
+		    
+		    Node head = new Node(sc.nextInt());
+		    Node tail = head;
+		    
+		    for(int i=1; i<n; i++){
+		        tail.next = new Node(sc.nextInt());
+		        tail = tail.next;
+		    }
+		    Solution obj = new Solution();
+		    head = obj.sort(head);
+		    printList(head);
+		}
+	}
+}
+
+// } Driver Code Ends
+
+
+
+/*
+class Node {
+    int data;
+    Node next;
+    
+    public Node (int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+*/
+
+class Solution {
+    
+   public Node sort(Node head){
+       ArrayList<Integer> list = new ArrayList<>();
+       Node temp = head;
+       while(temp != null){
+           list.add(temp.data);
+           temp = temp.next;
+       }
+       Collections.sort(list);
+       Node dummy = new Node(list.get(0));
+       Node curr = dummy;
+       for(int i=1; i<list.size(); i++){
+           curr.next = new Node(list.get(i));
+           curr = curr.next;
+       }
+       return dummy;
+   }
+}
+
+
+
+
+
+
